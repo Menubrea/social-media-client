ad21fe1c691a030e1bd53d2b7db734f78cc2c9a3
Truls Haakenstad <truls.haakenstad@outlook.com>
2022-11-05 20:33:31 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'mfrvmv',
-  pageLoadTimeout: 1000000000,
-  requestTimeout: 10000000000,
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
   },
