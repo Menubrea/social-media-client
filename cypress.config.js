d9229ee6ea205dfe46b1e35285d5cc7be91542a8
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-06 19:50:22 +0100

    updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   projectId: 'mfrvmv',
   requestTimeout: 10000000,
   responseTimeout: 10000000,
-  pageLoadTimeout: 1000000000,
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
   },
