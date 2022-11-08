011d74e728afedcfa18f8a0dd6aa1805f10d1254
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-08 08:38:40 +0100

    Updated wait time for e2e scripts

--- cypress/e2e/api/createPost.cy.js
+++ cypress/e2e/api/createPost.cy.js
@@ -7,7 +7,7 @@ describe('Social Media Client: Testing Create Post Form:', () => {
       .contains('Login')
       .should('not.be.hidden')
       .click()
-      .wait(1100);
+      .wait(850);
 
     cy.get("#loginForm input[type='email']")
       .should('not.be.disabled')
