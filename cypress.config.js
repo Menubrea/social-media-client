209a7805b0ea4a1d326ee1fad74b5c14e6afcb8a
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-08 09:52:26 +0100

    set up env in config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   projectId: 'mfrvmv',
   requestTimeout: 10000000,
   responseTimeout: 10000000,
+  env: {
+    email: 'menubreacypress@noroff.no',
+    password: '12345678',
+    faulty_email: 'menubreacypress',
+    faulty_password: '1234',
+  },
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
   },
